72405c2cdd3f24b52a9050773d0bf77189e43647
Luis Valverde <luis.valverde@outlook.com>
2019-05-20 18:27:44 -0700

    Change current value to (1.68 A)(1/1.41)(90%) = 1072 mA

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1102,13 +1102,13 @@
   #define HOLD_MULTIPLIER    0.5  // Scales down the holding current from run current
   #define INTERPOLATE       true  // Interpolate X/Y/Z_MICROSTEPS to 256
 
-  #define X_CURRENT          800  // rms current in mA. Multiply by 1.41 for peak current.
+  #define X_CURRENT         1072  // rms current in mA. Multiply by 1.41 for peak current.
   #define X_MICROSTEPS        16  // 0..256
 
-  #define Y_CURRENT          800
+  #define Y_CURRENT         1072
   #define Y_MICROSTEPS        16
 
-  #define Z_CURRENT          800
+  #define Z_CURRENT         1072
   #define Z_MICROSTEPS        16
 
   #define X2_CURRENT         800
@@ -1120,10 +1120,10 @@
   #define Z2_CURRENT         800
   #define Z2_MICROSTEPS       16
 
-  #define E0_CURRENT         800
+  #define E0_CURRENT        1072
   #define E0_MICROSTEPS       16
 
-  #define E1_CURRENT         800
+  #define E1_CURRENT        1072
   #define E1_MICROSTEPS       16
 
   #define E2_CURRENT         800
