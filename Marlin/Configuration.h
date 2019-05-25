24005e8aaf36241f4ae6059f70d170c7eadfeb16
Luis Valverde <luis.valverde@outlook.com>
2019-05-25 08:10:14 -0700

    Set Z_PROBE_OFFSET_FROM_EXTRUDER = 0 as a test

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -791,7 +791,7 @@
  */
 #define X_PROBE_OFFSET_FROM_EXTRUDER 26   // X offset: -left  +right  [of the nozzle]
 #define Y_PROBE_OFFSET_FROM_EXTRUDER 51   // Y offset: -front +behind [the nozzle]
-#define Z_PROBE_OFFSET_FROM_EXTRUDER  5   // Z offset: -below +above  [the nozzle]
+#define Z_PROBE_OFFSET_FROM_EXTRUDER  0   // Z offset: -below +above  [the nozzle]
 
 // Certain types of probes need to stay away from edges
 #define MIN_PROBE_EDGE 15
